530174e381f7e301808864d47ad089bf496659ef
Maxmillion McLaughlin <github@maxmclau.com>
2020-02-03 15:58:51 -0700

    Protect against floating point errors by truncating outputs

--- test.js
+++ test.js
@@ -4,18 +4,18 @@ const standardAtmosphere = require('.')
 test('zero', t => {
   const zero = standardAtmosphere(0, true)
 
-  t.is(zero.density, 1.2250058148851999)
-  t.is(zero.viscosity, 0.000017893720185530342)
-  t.is(zero.pressure, 101327.2683744314)
-  t.is(zero.ssound, 340.29794005764234)
+  t.is(zero.density.toFixed(6), (1.2250058148851999).toFixed(6))
+  t.is(zero.viscosity.toFixed(6), (0.000017893720185530342).toFixed(6))
+  t.is(zero.pressure.toFixed(6), (101327.2683744314).toFixed(6))
+  t.is(zero.ssound.toFixed(6), (340.29794005764234).toFixed(6))
   t.is(zero.temperature, 288.15)
 })
 
 test('si', t => {
   const five = standardAtmosphere(5000, false)
 
-  t.is(five.density, 0.0020480958257755003)
-  t.is(five.pressure, 1760.7965190535078)
-  t.is(five.ssound, 1097.091019912311)
-  t.is(five.temperature, 500.83918084184756)
+  t.is(five.density.toFixed(6), (0.0020480958257755003).toFixed(6))
+  t.is(five.pressure.toFixed(6), (1760.7965190535078).toFixed(6))
+  t.is(five.ssound.toFixed(6), (1097.091019912311).toFixed(6))
+  t.is(five.temperature.toFixed(6), (500.83918084184756).toFixed(6))
 })
