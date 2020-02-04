76b4514ccc1ffa155059e9ee52c83ddc51e81f86
Maxmillion McLaughlin <github@maxmclau.com>
2020-02-03 18:29:06 -0700

    Add typescript type definitions

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+declare namespace standardAtmosphere {
+	interface Options {
+		/**
+    Output properties of atmosphere in SI (metric) units. 
+		@default true
+		*/
+		readonly si?: boolean
+	}
+}
+
+/**
+@param altitude - Altitude to return properties from
+@example
+```
+import normalizeUrl = require('normalize-url');
+standardAtmosphere(30000)
+//=> {
+  temperature: 226.64962024973363,  // K (Kelvin)
+  density: 0.018012827594042123, // kg/m^3
+  pressure: 1171.9514784825592, // N/m^2
+  viscosity: 0.0000147602655462344, // N sec/m^2
+  ssound: 301.80574748785267 // m/sec
+}
+```
+*/
+
+declare interface AtmosphericProperties {
+  temperature: number, // K (Kelvin) // Ra (Rankine)
+	density: number, // kg/m^3 // slug/ft3
+	pressure:  number, // N/m^2 // lbf/ft^2
+	viscosity: number, // N sec/m^2 // lb sec/ft^2
+	ssound: number // m/sec // ft/sec
+}
+
+declare function standardAtmosphere(altitude: number, options?: boolean): AtmosphericProperties;
+
+export = standardAtmosphere
